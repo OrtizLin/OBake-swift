612183df29aa02c56386f1fa752a4e9057d531f7
Otis-TWDD <otis@twdd.com.tw>
2019-04-26 10:34:34 +0800

    Add size convert function.

--- OBake/OBTools/OBCalculatorModel.swift
+++ OBake/OBTools/OBCalculatorModel.swift
@@ -16,6 +16,15 @@ enum WeightType {
     case oz
 }
 
+enum SizeType {
+    case fourToSix
+    case fourToEight
+    case SixToEight
+    case SixToFour
+    case EightToSix
+    case EightToFour
+}
+
 class Weight {
     var kg: Double?
     var g: Double?
@@ -34,6 +43,7 @@ class Weight {
 
 protocol WeightConvertion {
     func weightConvertValue(type: WeightType , value: Double) -> Weight
+    func sizeConvertValue(type: SizeType, value: Weight) -> Weight
 }
 
 class WeightConvert: WeightConvertion {
@@ -45,19 +55,43 @@ class WeightConvert: WeightConvertion {
         var basic: Double = 0
         
         switch type {
-            case .kg:
-                basic = value
-            case .g:
-                basic = value / 1000
-            case .twCatty:
-                basic = value * 0.6
-            case .lb:
-                basic = value * 0.453
-            case .oz:
-                basic = value * 0.028
+        case .kg:
+            basic = value
+        case .g:
+            basic = value / 1000
+        case .twCatty:
+            basic = value * 0.6
+        case .lb:
+            basic = value * 0.453
+        case .oz:
+            basic = value * 0.028
         }
         
         let weight = Weight(kg: basic, g: basic * 1000, twCatty: basic * 1.66, lb: basic * 2.2, oz: basic * 35.2)
+        
+        return weight
+    }
+    
+    func sizeConvertValue(type: SizeType, value: Weight) -> Weight {
+        
+        var basic: Double = 0
+        
+        switch type {
+        case .fourToEight:
+            basic = 2.8
+        case .fourToSix:
+            basic = 1.8
+        case .SixToFour:
+            basic = 0.6
+        case .SixToEight:
+            basic = 1.78
+        case .EightToFour:
+            basic = 0.4
+        case .EightToSix:
+            basic = 0.56
+        }
+        
+        let weight = Weight(kg: value.kg! * basic, g: value.g! * basic, twCatty: value.twCatty! * basic, lb: value.lb! * basic, oz: value.oz! * basic)
         
         return weight
     }
