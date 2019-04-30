c2561792252cd0fc9cfe54fc0ae77a8578b61e00
Otis-TWDD <otis@twdd.com.tw>
2019-04-30 15:55:50 +0800

    Add teacher info tableView

---
+++ OBake/OBTools/TeacherModel.swift
@@ -0,0 +1,15 @@
+//
+//  TeacherModel.swift
+//  OBake
+//
+//  Created by Otis on 2019/4/30.
+//  Copyright © 2019 Otis. All rights reserved.
+//
+
+import Foundation
+
+struct TeacherData {
+    let Name : String
+    let Class : String
+    let ImageUrl : String
+}
---
+++ OBake/OBTools/TeacherTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherTableViewCell.swift
+//  OBake
+//
+//  Created by Otis on 2019/4/30.
+//  Copyright © 2019 Otis. All rights reserved.
+//
+
+import UIKit
+
+class TeacherTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var resumeLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
+
