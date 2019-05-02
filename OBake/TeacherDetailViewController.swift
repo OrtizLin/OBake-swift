087fac8df8bb76e1b02162a58b1545133e122e8d
Otis-TWDD <otis@twdd.com.tw>
2019-05-02 11:37:36 +0800

    Teacher detail view.

--- OBake/TeacherDetailViewController.swift
+++ OBake/TeacherDetailViewController.swift
@@ -12,15 +12,15 @@ import Toast_Swift
 
 class TeacherDetailViewController: UIViewController {
     
+    
     @IBOutlet weak var detailScrollView: UIScrollView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var experienceLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
-    
-    
-    
     @IBOutlet weak var searchButton: UIButton!
+    
+    var teacherIndex: Int?
     
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -28,15 +28,15 @@ class TeacherDetailViewController: UIViewController {
         detailScrollView.contentSize.width = 0
         detailScrollView.isDirectionalLockEnabled = true
         searchButton.themeButtonStyle(isSolid: false)
-        nameLabel.text = "姓名：康小龍"
-        titleLabel.text = "稱號：雙北最好喝飲料"
-        let str = "經歷：美而美餐飲集團積極轉型，除旗下早餐品牌「瑞麟美而美」推出全新旗艦店型，更跨足冰品與手搖飲料，推出「康青龍」"
+        nameLabel.text = TeacherManager.shared.getFakeTeacherArray()[teacherIndex ?? 0].Name
+        titleLabel.text = "稱號：" + TeacherManager.shared.getFakeTeacherArray()[teacherIndex ?? 0].Title
+        let str = "經歷：\n" + TeacherManager.shared.getFakeTeacherArray()[teacherIndex ?? 0].Resume
         let paraph = NSMutableParagraphStyle()
         paraph.lineSpacing = 5
         let attributes = [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 12),
                           NSAttributedString.Key.paragraphStyle: paraph]
-        experienceLabel.attributedText = NSAttributedString(string: str, attributes: attributes)
-        photoImageView?.loadImageUsingCacheWithUrlString(urlString:"https://i.imgur.com/og2lmPH.jpg")
+        experienceLabel.attributedText = NSAttributedString(string: str , attributes: attributes)
+        photoImageView?.loadImageUsingCacheWithUrlString(urlString:TeacherManager.shared.getFakeTeacherArray()[teacherIndex ?? 0].ImageUrl )
         photoImageView?.contentMode = .scaleAspectFit
     }
     
--- OBake/TeacherViewController.swift
+++ OBake/TeacherViewController.swift
@@ -14,7 +14,7 @@ class TeacherViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad(){
         super.viewDidLoad()
-        teachers = getFakeDataArray()
+        teachers = TeacherManager.shared.getFakeTeacherArray()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 22, bottom: 0, right: 22)
@@ -35,7 +35,7 @@ extension TeacherViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TeacherTableViewCell", for: indexPath) as! TeacherTableViewCell
         cell.nameLabel.text = teachers?[indexPath.row].Name
-        let str = "經歷: \n" + (teachers?[indexPath.row].Class)!
+        let str = "經歷: \n" + (teachers?[indexPath.row].Resume)!
         let paraph = NSMutableParagraphStyle()
         paraph.lineSpacing = 5
         let attributes = [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 12),
@@ -64,22 +64,16 @@ extension TeacherViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
-    self.performSegue(withIdentifier: "teacherDetail", sender: nil)
+    self.performSegue(withIdentifier: "teacherDetail", sender: indexPath)
      
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = sender as? IndexPath,
+            segue.identifier == "teacherDetail" {
+            let viewController = segue.destination as! TeacherDetailViewController
+            viewController.teacherIndex = indexPath.row
+        }
     }
 }
 
-extension TeacherViewController {
-    func getFakeDataArray() -> [TeacherData] {
-        let dataOne = TeacherData(Name: "李大華", Class: "法國藍帶美國舊金山分校第一名畢業. 義大利Sorrento專研義式傳統料理. 美國紐約ICE(Institute of Culinary Education)烹飪學校學習甜點", ImageUrl: "https://i.imgur.com/15vFKUC.jpg")
-        let dataSecond = TeacherData(Name: "林中強", Class: "2018 T.U.C.C 泰國廚藝挑戰賽,西式職業組個人現場烹調. 1.鯖魚烹調（銅牌）2.羊肉烹調（銅牌）3.亞洲菜烹調（佳作）4.豬雞雙拼烹調（銀牌）", ImageUrl: "https://i.imgur.com/MO5fvJa.jpg")
-        let dataThird = TeacherData(Name: "康小龍", Class: "美而美餐飲集團積極轉型，除旗下早餐品牌「瑞麟美而美」推出全新旗艦店型，更跨足冰品與手搖飲料，推出「康青龍」", ImageUrl: "https://i.imgur.com/og2lmPH.jpg")
-        
-        var dataArray = [TeacherData]()
-        dataArray.append(dataOne)
-        dataArray.append(dataSecond)
-        dataArray.append(dataThird)
-        
-        return dataArray
-    }
-}
+
